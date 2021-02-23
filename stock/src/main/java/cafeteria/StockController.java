c34b9bd76848386471751eeb96039a993f75fe33
P085860 <P085860@SKTP085860PN002.SKT.AD>
2021-02-23 14:34:57 +0900

    useStock를 위한 Controller 수정

--- stock/src/main/java/cafeteria/StockController.java
+++ stock/src/main/java/cafeteria/StockController.java
@@ -1,7 +1,9 @@
 package cafeteria;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+
+@RequestMapping("/stocks")
  @RestController
  public class StockController {
 
-
+	@Autowired
+	private StockRepository stockRepository;
+	
+	@PatchMapping("/useStock")
+	public Stock usedStock(@RequestBody Ordered ordered) {
+		List<Stock> stocks = stockRepository.findByProductName(ordered.getProductName());
+		int s = ordered.getQty();
+		Stock stock = stocks.get(0);
+		stock.setQty(stock.getQty() - s);
+		
+		if(stock.getQty() < 0) throw new RuntimeException("Stock is not enough.");
+		stockRepository.save(stock);
+		return stock;
+    }
  }

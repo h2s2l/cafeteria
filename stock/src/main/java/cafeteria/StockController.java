642b7d1660fe2c7a795ee0358c80c8efd8256e1e
P085860 <P085860@SKTP085860PN002.SKT.AD>
2021-02-23 15:14:29 +0900

    PATCH -> PUT으로 변경

--- stock/src/main/java/cafeteria/StockController.java
+++ stock/src/main/java/cafeteria/StockController.java
@@ -1,14 +1,11 @@
 package cafeteria;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.util.List;
 
 
@@ -19,7 +16,7 @@ import java.util.List;
 	@Autowired
 	private StockRepository stockRepository;
 	
-	@PatchMapping("/useStock")
+	@PutMapping("/useStock")
 	public Stock usedStock(@RequestBody Stock stock) {
 		List<Stock> stocks = stockRepository.findByProductName(stock.getProductName());
 		int s = stock.getQty();

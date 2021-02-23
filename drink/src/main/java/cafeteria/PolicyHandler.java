6a2f4fa686498c47124b59ec6aaac652589e011c
P085860 <P085860@SKTP085860PN002.SKT.AD>
2021-02-23 17:12:44 +0900

    Drink saga를 위한 CancelFailed 추가

--- drink/src/main/java/cafeteria/PolicyHandler.java
+++ drink/src/main/java/cafeteria/PolicyHandler.java
@@ -10,58 +10,66 @@ import org.springframework.stereotype.Service;
 import cafeteria.config.kafka.KafkaProcessor;
 
 @Service
-public class PolicyHandler{
-	
+public class PolicyHandler {
+
 	@Autowired
-    private DrinkRepository drinkRepository;
-	
-    @StreamListener(KafkaProcessor.INPUT)
-    public void onStringEventListener(@Payload String eventString){
+	private DrinkRepository drinkRepository;
 
-    }
-    
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverOrdered_(@Payload Ordered ordered){
+	@StreamListener(KafkaProcessor.INPUT)
+	public void onStringEventListener(@Payload String eventString) {
 
-        if(ordered.isMe()){
-            System.out.println("##### listener  : " + ordered.toJson());
-            
-            List<Drink> drinks = drinkRepository.findByOrderId(ordered.getId());
-            for(Drink drink : drinks) {
-           	drink.setPhoneNumber(ordered.getPhoneNumber());
-            	drink.setProductName(ordered.getProductName());
-               	drink.setQty(ordered.getQty());
-               	drinkRepository.save(drink);
-            }
-            
-            
-        }
-    }
+	}
 
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverPaymentApproved_(@Payload PaymentApproved paymentApproved){
+	@StreamListener(KafkaProcessor.INPUT)
+	public void wheneverOrdered_(@Payload Ordered ordered) {
 
-        if(paymentApproved.isMe()){
-            System.out.println("##### listener  : " + paymentApproved.toJson());
-            
-            Drink drink = new Drink();
-            drink.setOrderId(paymentApproved.getOrderId());
-            drink.setStatus(paymentApproved.getStatus());
-            drinkRepository.save(drink);
-        }
-    }
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverPaymentCanceled_(@Payload PaymentCanceled paymentCanceled){
+		if (ordered.isMe()) {
+			System.out.println("##### listener  : " + ordered.toJson());
 
-        if(paymentCanceled.isMe()){
-            System.out.println("##### listener  : " + paymentCanceled.toJson());
-            
-            List<Drink> drinks = drinkRepository.findByOrderId(paymentCanceled.getOrderId());
-            for(Drink drink : drinks) {
-            	drink.setStatus("DrinkCancled");
-            	drinkRepository.save(drink);
-            }
-        }
-    }
+			List<Drink> drinks = drinkRepository.findByOrderId(ordered.getId());
+			for (Drink drink : drinks) {
+				drink.setPhoneNumber(ordered.getPhoneNumber());
+				drink.setProductName(ordered.getProductName());
+				drink.setQty(ordered.getQty());
+				drinkRepository.save(drink);
+			}
+
+		}
+	}
+
+	@StreamListener(KafkaProcessor.INPUT)
+	public void wheneverPaymentApproved_(@Payload PaymentApproved paymentApproved) {
+
+		if (paymentApproved.isMe()) {
+			System.out.println("##### listener  : " + paymentApproved.toJson());
+
+			Drink drink = new Drink();
+			drink.setOrderId(paymentApproved.getOrderId());
+			drink.setStatus(paymentApproved.getStatus());
+			drinkRepository.save(drink);
+		}
+	}
+
+	@StreamListener(KafkaProcessor.INPUT)
+	public void wheneverOrderCanceled_(@Payload OrderCanceled orderCanceled) {
+
+		if (orderCanceled.isMe()) {
+			System.out.println("##### listener  : " + orderCanceled.toJson());
+
+			List<Drink> drinks = drinkRepository.findByOrderId(orderCanceled.getId());
+			if (drinks.stream().filter(drink -> !"PaymentApproved".equals(drink.getStatus())).count() > 0) {
+				CancelFailed fail = new CancelFailed();
+				fail.setOrderId(orderCanceled.getId());
+				fail.setPhoneNumber(orderCanceled.getPhoneNumber());
+				fail.publish();
+			} else {
+
+				for (Drink drink : drinks) {
+					drink.setStatus("DrinkCancled");
+					drinkRepository.save(drink);
+				}
+			}
+		}
+	}
 
 }
